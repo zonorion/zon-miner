35e1bd6e0a45f0eec133fb4093a24150290fb90a
Zon <tan.m.hoang@vntrip.vn>
2017-09-23 11:21:26 +0700

    add bot

--- src/app.js
+++ src/app.js
@@ -2,28 +2,46 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 const CoinHive = require('coin-hive');
+import cluster from 'cluster';
+const numCPUs = require('os').cpus().length;
 
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+if (cluster.isMaster) {
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+} else {
+    const app = express();
+    app.use(cors());
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
 
-app.listen(process.env.PORT || 5000, () => {
-    console.log(`HTTP server is listening at: ${process.env.PORT || 5000}`);
-    (async () => {
-        const miner = await CoinHive('yhh5a4CEYpQgw2aQFoHAfNB8ZCdWzbRY');
-        await miner.start();
-        miner.on('found', () => console.log('Found!'));
-        miner.on('accepted', () => console.log('Accepted!'));
-        miner.on('update', data => console.log(`
+    app.listen(process.env.PORT || 5000, () => {
+        console.log(`HTTP server is listening at: ${process.env.PORT || 5000}`);
+        (async () => {
+            try {
+                const miner = await CoinHive('yhh5a4CEYpQgw2aQFoHAfNB8ZCdWzbRY');
+                await miner.start();
+                miner.on('found', () => console.log('Found!'));
+                miner.on('accepted', () => console.log('Accepted!'));
+                miner.on('update', data => console.log(`
             Hashes per second: ${data.hashesPerSecond}
             Total hashes: ${data.totalHashes}
             Accepted hashes: ${data.acceptedHashes}`));
-        // setTimeout(async () => await miner.stop(), 60000);
-    })();
-});
-
-app.get('*', (req, res) => {
-    res.status(200)
-        .json({message: 'Successfully!'})
+            } catch (e) {
+                console.log(e);
+            }
+        })();
+    });
+    app.get('*', (req, res) => {
+        try {
+            res.status(200)
+                .json({message: 'Successfully!'})
+        } catch (e) {
+            res.json({message: 'Oops! Somethings wrong...'})
+        }
+    });
+}
+cluster.on('exit', function(worker, code, signal) {
+    console.log('Worker %d died with code/signal %s. Restarting worker...', worker.process.pid, signal || code);
+    cluster.fork();
 });
